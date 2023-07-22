1c78494cbb5c633fa9f06aa53fa34b60e4ebba61
27Lia <117743861+27Lia@users.noreply.github.com>
2023-07-23 01:37:11 +0900

    keydown + toggle 기능구현

--- index.js
+++ index.js
@@ -5,20 +5,39 @@ const clickKey = () => {
   pianokeys.forEach((key) => {
     let keyData = key.dataset.key
     allkeys.push(keyData)
-    key.addEventListener("click", () => play(keyData)); // 각 키를 클릭하면 해당하는 키의 데이터 키 값을 play 함수로 전달
+    key.addEventListener("click", () => play(keyData)); 
     });
   };
+  clickKey();
+  
 
-  const keydownHandler = (e) => {
-    let keyData = e.key;
-    play(keyData);
-  };
 
 function play(key) {
   const audio = new Audio(`/piano/${key}.wav`); // 클릭된 키의 데이터 키 값을 사용하여 오디오 파일의 경로를 설정
   audio.play(); // 오디오 재생
+  const keydown = document.querySelector(`[data-key="${key}"]`);
+  keydown.classList.add("active") 
+  setTimeout(() => { 
+    keydown.classList.remove("active");
+}, 150);
 }
+
+const keydownHandler = (e) => {
+  let keyData = e.key;
+  play(keyData);
+};
+
 window.addEventListener("keydown", keydownHandler);
 
-clickKey()
+
+const circle = document.querySelector(".circle");
+circle.addEventListener("click", circleClick);
+
+function circleClick() {
+  circle.classList.toggle("toggle-active"); 
+  const checkbox = document.querySelector(".checkbox");
+  checkbox.classList.toggle("checkbox-active")
+}
+
+
 

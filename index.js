14bb8a17a978c74a30854856c6976a76588fc984
27Lia <117743861+27Lia@users.noreply.github.com>
2023-07-23 03:21:23 +0900

    -

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const pianokeys = document.querySelectorAll(".key") 
+const pianokeys = document.querySelectorAll(".key")
 const allkeys = []; // 모든 키 배열
-const audio = new Audio(); 
+const audio = new Audio();
 const volumeSlider = document.querySelector(".volume input");
 
 const clickKey = () => {
   pianokeys.forEach((key) => {
     let keyData = key.dataset.key
     allkeys.push(keyData)
-    key.addEventListener("click", () => play(keyData)); 
-    });
-  };
-  clickKey();
+    key.addEventListener("click", () => play(keyData));
+  });
+};
+clickKey();
+
 
 function play(key) {
   audio.src = `/piano/${key}.wav`; // 클릭된 키의 데이터 키 값을 사용하여 오디오 파일의 경로를 설정
   audio.play(); // 오디오 재생
   const keydown = document.querySelector(`[data-key="${key}"]`);
-  keydown.classList.add("active") 
-  setTimeout(() => { 
+  keydown.classList.add("active")
+  setTimeout(() => {
     keydown.classList.remove("active");
-}, 150);
+  }, 150);
 }
 
 const keydownHandler = (e) => {
@@ -36,7 +37,7 @@ const hiddenItems = document.querySelectorAll("li > span");
 checkBox.addEventListener("click", (e) => {
   const isChecked = e.target.checked;
   circle.style.transform = isChecked ? "translateX(2.5rem)" : "translateX(0rem)";
-  hiddenItems.forEach(function(el) {
+  hiddenItems.forEach(function (el) {
     if (isChecked) {
       el.classList.add("hidden");
     } else {
